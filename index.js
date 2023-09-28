0cdf54b57550444486e9e64a2225f2e131cc67fb
linda wanjiku <lindashykooh@gmail.com>
2023-09-29 01:49:09 +0300

    completed asignments

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    const thankYouMessages = []; 
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message); 
+    }
+  
+    return thankYouMessages;
+  }
+  
+  const names = ["Alice", "Bob", "Charlie"];
+  const eventName = "birthday";
+  const messages = writeCards(names, eventName);
+  console.log(messages);
+  function countDown(number) {
+    if (number >= 0) {
+      for (let i = number; i >= 0; i--) {
+        console.log(i);
+      }
+    } else {
+      console.log("Please provide a non-negative number.");
+    }
+  }
+  
+  countDown(5); 
